4ba7f3b12c1a1845a9742bfd1d120e46a133e592
leandromalves <lmouralves@gmail.com>
2018-06-30 14:43:06 -0300

    adicionando pom e demais resources

---
+++ src/main/java/br/com/imusica/desafio/DesafioImusicaWebApplication.java
@@ -0,0 +1,12 @@
+package br.com.imusica.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioImusicaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioImusicaWebApplication.class, args);
+	}
+}
